328b77d792b10ad39b10b0102bffa670d47d9206
osangu <osangu012@gmail.com>
2022-12-05 21:55:31 +0900

    ADD: Date type changer

--- app/util/type_changer.py
+++ app/util/type_changer.py
@@ -1,5 +1,21 @@
-from datetime import date
+from datetime import date, datetime
+
+
+def date_to_str(from_date: date):
+    return from_date.strftime('%Y%m%d').replace('-', '')
 
 
 def date_to_int(from_data: date):
-    return int(from_data.strftime('%Y%m%d').replace('-', ''))
+    return int(date_to_str(from_date=from_data))
+
+
+def int_to_date(yymmdd: int):
+    return datetime.strptime(str(yymmdd), '%Y%m%d').date()
+
+
+def int_to_week_of_day(yymmdd: int):
+    week_of_day = {0: '월요일', 1: '화요일', 2: '수요일', 3: '목요일', 4: '금요일', 5: '토요일', 6: '일요일'}
+
+    index = datetime.strptime(str(yymmdd), '%Y%m%d').date().weekday()
+
+    return week_of_day[index]
