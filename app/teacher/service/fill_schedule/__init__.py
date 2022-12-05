05700db220621524bd5149c834ee7e4b2de1fa06
osangu <osangu012@gmail.com>
2022-12-05 21:54:05 +0900

    NEW START

--- app/teacher/service/fill_schedule/__init__.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class TeacherFillScheduleService(ABC):
-
-    @abstractmethod
-    def fill_schedule(self, grade: int, class_: int, start_date: int, end_date: int):
-        pass
