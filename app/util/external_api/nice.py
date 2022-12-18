a2629d2e9c5cc0a0ba311f5b1a6df35ef6a7974d
osangu <osangu012@gmail.com>
2022-12-18 16:41:40 +0900

    update: 시간표 분할 해서 가져오기

--- app/util/external_api/nice.py
+++ app/util/external_api/nice.py
@@ -1,19 +1,20 @@
+from datetime import date
 from requests import get
 
 from app.config import Config
 
-from app.util import ktc_this_monday, ktc_this_friday
+from app.util import ktc_this_monday, ktc_this_friday, ktc_next_monday, ktc_next_friday
 from app.util.type_changer import int_to_week_of_day, date_to_int
 
 grade = [1, 2, 3]
 class_num = [1, 2, 3, 4]
 
 
-def get_schedule_list(grade: int, _class: int, start_date: int, end_date: int):
+def get_schedule_list(grade: int, class_: int, start_date: int, end_date: int):
     return get(Config.Nice.URL.format(
         ACCESS_KEY=Config.Nice.ACCESS_KEY,
         GRADE=grade,
-        _CLASS=_class,
+        _CLASS=class_,
         START_DATE=start_date,
         END_DATE=end_date
     )).json()['hisTimetable'][1]['row']
@@ -30,15 +31,25 @@ def parse_schedule(schedule_list):
     }, schedule_list))
 
 
-def get_this_weekend_schedule():
-    all_schedule_list = []
+def get_this_week_schedule():
     monday, friday = ktc_this_monday(), ktc_this_friday()
 
+    return get_all_class_schedule(monday, friday)
+
+
+def get_next_week_schedule():
+    monday, friday = ktc_next_monday(), ktc_next_friday()
+
+    return get_all_class_schedule(monday, friday)
+
+
+def get_all_class_schedule(monday: date, friday: date):
+    all_schedule_list = []
     for g in grade:
         for c in class_num:
             all_schedule_list += parse_schedule(
                 get_schedule_list(
-                    grade=g, _class=c, start_date=date_to_int(monday), end_date=date_to_int(friday)
+                    grade=g, class_=c, start_date=date_to_int(monday), end_date=date_to_int(friday)
                 )
             )
 
