3680891a2d7469cff04390829ccd525129659caf
osangu <osangu012@gmail.com>
2022-12-19 12:22:12 +0900

    update: schedule 세분화

---
+++ app/core/schedule/service/fill_schedule.py
@@ -0,0 +1,35 @@
+from sqlalchemy.exc import IntegrityError
+
+from app.util.dao.mysql.schedule.command import insert_schedule_by_sql
+from app.util.external_api.nice import get_this_week_schedule, get_next_week_schedule
+
+
+def fill_two_week_schedule():
+    for schedule in [
+        get_this_week_schedule(),
+        get_next_week_schedule()
+    ]:
+        sql = __get_insert_sql(schedule)
+
+        try:
+            insert_schedule_by_sql(sql)
+
+        except IntegrityError:
+            continue
+
+
+def __get_insert_sql(schedule: list):
+    sql, values = 'INSERT INTO tbl_schedule(gradations, grade, class_num, name, day_at, week_of_day) VALUES', ' '
+
+    values = values.join(
+        map(
+            lambda i: " ('{}','{}','{}','{}','{}','{}'),".format(
+                i['SEQUENCE'], i['GRADE'], i['ROOM'], i['SUBJECT'], i['DAY'], i['WEEK_OF_DAY']
+            )
+            , schedule
+        )
+    )
+
+    sql += values[:-1] + ';'
+
+    return sql
---
+++ app/core/schedule/service/get_schedule_list.py
@@ -0,0 +1,23 @@
+from datetime import date, timedelta
+
+from app.util.exception.custom import DatePeriodIsNotWeek
+from app.util.dao.mysql.schedule.query import query_schedule_list
+
+
+def get_schedule_list(grade: str, room: str, start_at: date, end_at: date):
+    __check_week_period(start_at, end_at)
+
+    response = {}
+
+    for i in query_schedule_list(grade, room, start_at, end_at):
+        if i['day_at'].weekday() in response:
+            response[i['day_at'].weekday()] += [i]
+        else:
+            response[i['day_at'].weekday()] = [i]
+
+    return response
+
+
+def __check_week_period(start_at: date, end_at: date):
+    if start_at + timedelta(days=6) < end_at or start_at < end_at - timedelta(days=6):
+        raise DatePeriodIsNotWeek
