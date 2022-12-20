eb3d53cf9d46b896f5da7ef3eca1b6c10e8c992c
osangu <osangu012@gmail.com>
2022-12-20 20:07:18 +0900

    update: name 변수명

--- app/util/dao/mysql/schedule/__init__.py
+++ app/util/dao/mysql/schedule/__init__.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from sqlalchemy import Column, CHAR, VARCHAR, DATE, BINARY, PrimaryKeyConstraint
+from sqlalchemy.orm import synonym
 
 from sqlalchemy.ext.hybrid import hybrid_property
 
@@ -12,20 +13,12 @@ class Schedule(Base):
 
     grade = Column('grade', CHAR(1), nullable=False)
     class_num = Column('class_num', CHAR(1), nullable=False)
-    _name = Column('name', VARCHAR(25), nullable=False)
+    name = Column('name', VARCHAR(25), nullable=False)
     gradations = Column('gradations', CHAR(1), nullable=False)
     day_at = Column('day_at', DATE, nullable=False)
     week_of_day = Column(CHAR(3), nullable=False)
 
-    @hybrid_property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, new_name: str):
-        self._name = new_name
-
     __table_args__ = (
-        PrimaryKeyConstraint(grade, class_num, _name, gradations, day_at),
+        PrimaryKeyConstraint(grade, class_num, name, gradations, day_at),
         {}
     )
