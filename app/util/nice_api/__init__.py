e82b4a9b2ab989378df7d58b2f757bb0aabc4f6e
osangu <osangu012@gmail.com>
2022-12-21 16:33:47 +0900

    update: 자잘한 수정

--- app/util/nice_api/__init__.py
+++ app/util/nice_api/__init__.py
@@ -6,6 +6,7 @@ from app.config import Config
 from app.util import ktc_this_monday, ktc_this_friday, ktc_next_monday, ktc_next_friday
 from app.util.type_changer import int_to_week_of_day, date_to_int
 
+Config = Config.Nice
 grade = [1, 2, 3]
 class_num = [1, 2, 3, 4]
 
@@ -24,11 +25,13 @@ def get_next_week_schedule():
 
 def get_all_class_schedule(monday: date, friday: date):
     all_schedule_list = []
+    monday, friday = date_to_int(monday), date_to_int(friday)
+
     for g in grade:
         for c in class_num:
             all_schedule_list += parse_schedule(
                 get_schedule_list(
-                    grade=g, class_=c, start_date=date_to_int(monday), end_date=date_to_int(friday)
+                    grade=g, class_=c, start_date=monday, end_date=friday
                 )
             )
 
@@ -47,10 +50,12 @@ def parse_schedule(schedule_list):
 
 
 def get_schedule_list(grade: int, class_: int, start_date: int, end_date: int):
-    return get(Config.Nice.URL.format(
-        ACCESS_KEY=Config.Nice.ACCESS_KEY,
-        GRADE=grade,
-        _CLASS=class_,
-        START_DATE=start_date,
-        END_DATE=end_date
-    )).json()['hisTimetable'][1]['row']
+    return get(
+        Config.URL.format(
+            ACCESS_KEY=Config.ACCESS_KEY,
+            GRADE=grade,
+            CLASS=class_,
+            START_DATE=start_date,
+            END_DATE=end_date
+        )
+    ).json()['hisTimetable'][1]['row']
