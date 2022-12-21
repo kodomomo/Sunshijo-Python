568fc12094e0899fc9407fc8cff75c2a4875671a
osangu <osangu012@gmail.com>
2022-12-21 15:56:05 +0900

    update: 위치 바꿈

--- app/util/nice_api/__init__.py
+++ app/util/nice_api/__init__.py
@@ -10,27 +10,6 @@ grade = [1, 2, 3]
 class_num = [1, 2, 3, 4]
 
 
-def get_schedule_list(grade: int, class_: int, start_date: int, end_date: int):
-    return get(Config.Nice.URL.format(
-        ACCESS_KEY=Config.Nice.ACCESS_KEY,
-        GRADE=grade,
-        _CLASS=class_,
-        START_DATE=start_date,
-        END_DATE=end_date
-    )).json()['hisTimetable'][1]['row']
-
-
-def parse_schedule(schedule_list):
-    return list(map(lambda x: {
-        'SUBJECT': x['ITRT_CNTNT'],
-        'GRADE': x['GRADE'],
-        'ROOM': x['CLASS_NM'],
-        'SEQUENCE': x['PERIO'],
-        'WEEK_OF_DAY': int_to_week_of_day(x['ALL_TI_YMD']),
-        'DAY': x['ALL_TI_YMD'],
-    }, schedule_list))
-
-
 def get_this_week_schedule():
     monday, friday = ktc_this_monday(), ktc_this_friday()
 
@@ -54,3 +33,24 @@ def get_all_class_schedule(monday: date, friday: date):
             )
 
     return all_schedule_list
+
+
+def parse_schedule(schedule_list):
+    return list(map(lambda x: {
+        'SUBJECT': x['ITRT_CNTNT'],
+        'GRADE': x['GRADE'],
+        'ROOM': x['CLASS_NM'],
+        'SEQUENCE': x['PERIO'],
+        'WEEK_OF_DAY': int_to_week_of_day(x['ALL_TI_YMD']),
+        'DAY': x['ALL_TI_YMD'],
+    }, schedule_list))
+
+
+def get_schedule_list(grade: int, class_: int, start_date: int, end_date: int):
+    return get(Config.Nice.URL.format(
+        ACCESS_KEY=Config.Nice.ACCESS_KEY,
+        GRADE=grade,
+        _CLASS=class_,
+        START_DATE=start_date,
+        END_DATE=end_date
+    )).json()['hisTimetable'][1]['row']
