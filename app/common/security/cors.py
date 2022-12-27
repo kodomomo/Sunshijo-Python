0628c00671de15e54d3056a419b7c7beb714aac2
osangu <osangu012@gmail.com>
2022-12-27 18:53:30 +0900

    udpate: cors

--- app/common/security/cors.py
+++ app/common/security/cors.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def initialize_cors(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["localhost:3000", "http://localhost:3000/main", "http://localhost:3000", "*"],
+        allow_origins=["http://localhost:3000", "http://localhost:3000/main", "http://127.0.0.1:3000", "*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
